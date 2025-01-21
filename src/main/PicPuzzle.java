5846bef90512497e94efd30f1a4ce0373fd58b45
Rylle <verdaderkarylle@gmail.com>
2025-01-21 21:13:24 +0800

    deleted a panel and fixed panel layouts

--- src/main/PicPuzzle.java
+++ src/main/PicPuzzle.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class PicPuzzle{
     JFrame frm = new JFrame();
     JLabel lbl1, lbl2;
-    JPanel mainPanel, topPanel, centerPanel, leftPanel, rightPanel, bottomLeftPanel, bottomRightPanel;
+    JPanel mainPanel, topPanel, centerPanel, leftPanel, rightPanel, bottomPanel;
     JButton icon, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, picture;
 
     public PicPuzzle(){
@@ -14,12 +14,13 @@ public class PicPuzzle{
         //top panel, center panel container: main panel 
         mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());    
-        mainPanel.setSize(750, 700);
+        mainPanel.setSize(950, 700);
         mainPanel.setBackground(Color.RED);
 
         //Label 1 & 2
         lbl1 = new JLabel("Click for change icon ->");
-        lbl2 = new JLabel("Click for change picture ↑");
+        lbl2 = new JLabel("Click for change picture ↑                                "); //kung ayaw mag center edi im doing ts my own way LOL
+        //tinatamad ako mag layout para sa lbl2 kaya ganito na lang 
 
         //Right Icon
         icon = new JButton();
@@ -31,7 +32,7 @@ public class PicPuzzle{
 
         //lbl1 and icon container: top panel
         topPanel = new JPanel(new BorderLayout());
-        topPanel.setBorder(BorderFactory.createEmptyBorder(15,30,0,30));
+        topPanel.setBorder(BorderFactory.createEmptyBorder(15,50,0,50));
         topPanel.add(topWrapper, BorderLayout.WEST);
         topPanel.add(icon, BorderLayout.EAST);
 
@@ -51,7 +52,7 @@ public class PicPuzzle{
         leftPanel = new  JPanel();
         leftPanel.setLayout(new GridLayout(3, 3));
         leftPanel.setPreferredSize(new Dimension(200, 200));        
-        leftPanel.setBorder(BorderFactory.createEmptyBorder(20,30,20,30));
+        leftPanel.setBorder(BorderFactory.createEmptyBorder(35,50,45,50));
 
         //add to left panel
         leftPanel.add(btn1);
@@ -71,40 +72,41 @@ public class PicPuzzle{
         //picture container: right panel 
         rightPanel = new JPanel();  
         rightPanel.setLayout(new BorderLayout());
-        rightPanel.setBorder(BorderFactory.createEmptyBorder(20,30,20,30));  
+        rightPanel.setBorder(BorderFactory.createEmptyBorder(35,50,45,50));  
         rightPanel.add(picture, BorderLayout.CENTER);   
-        
-        //bottom left panel(empty)
-        bottomLeftPanel = new JPanel(); 
+                
 
         //wrapping the text para hindi siya mag compress sa taas(idk ayaw niya mapunta sa taas pag wala wrapper and gridbaglayout)
-        JPanel centerWrapper = new JPanel(new GridBagLayout());
+        JPanel leftWrapper = new JPanel(new GridBagLayout());
 
         //lbl2 container: bottom right panel
-        bottomRightPanel = new JPanel(new BorderLayout());
-        centerWrapper.add(lbl2);
-        bottomRightPanel.add(centerWrapper, BorderLayout.NORTH);   
+        bottomPanel = new JPanel(); 
+        bottomPanel = new JPanel(new BorderLayout());
+        leftWrapper.add(lbl2);
+        bottomPanel.add(leftWrapper, BorderLayout.EAST);   
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(0,0,30,50));   
 
-        //left panel, right panel, bottom left panel, bottom right panel container: center panel    
+        //left panel, right panel, bottom panel container: center panel    
         centerPanel = new JPanel();
-        centerPanel.setLayout(new GridLayout(2, 2));
+        centerPanel.setLayout(new GridLayout(1, 2));
         centerPanel.add(leftPanel);
         centerPanel.add(rightPanel);
-        centerPanel.add(bottomLeftPanel);
-        centerPanel.add(bottomRightPanel);
+        centerPanel.add(bottomPanel);
 
         //add to main panel
         mainPanel.add(topPanel, BorderLayout.NORTH); 
-        mainPanel.add(centerPanel, BorderLayout.CENTER);    
+        mainPanel.add(centerPanel, BorderLayout.CENTER);
+        mainPanel.add(bottomPanel, BorderLayout.SOUTH);
+           
 
         //add to frame
         frm.add(mainPanel);
 
         //frame properties  
         frm.setTitle("PicPuzzle");
-        frm.setSize(750, 700);
+        frm.setSize(1150, 750);
         frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frm.setResizable(false);    
+        frm.setResizable(true);    
         frm.setLocation(300, 80);
         frm.setVisible(true);
     }
