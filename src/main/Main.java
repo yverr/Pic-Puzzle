85fafb114f940d3de97b12a5da4d7ba03206420e
rylle <164985874+yverr@users.noreply.github.com>
2025-03-03 11:36:57 +0800

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,5 +2,8 @@ public class Main{
     public static void main(String[] args){
         GameMenu g = new GameMenu();
         g.getClass();
+
+        BackgroundMusic bgm = new BackgroundMusic();
+        bgm.bgMusic();
     }
 }
